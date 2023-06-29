919f178e9a7fb4947e955cc7291c10cccbb8a30a
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-06-30 01:01:15 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWrapper = document.querySelector('.gallery');
+
+const galleryList = createGallery (galleryItems);
+
+function createGallery (arr) {
+ return arr.map(({preview, original, description }) => 
+     `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">    <img
+           class="gallery__image"
+          src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+       </a>
+  </li>` 
+).join(' ')
+
+}
+
+galleryWrapper.insertAdjacentHTML('beforeend', galleryList)
+
+galleryWrapper.addEventListener('click', onImgClick)
+
+
+
+function onImgClick (evt) {
+evt.preventDefault()
+document.addEventListener('keydown', onPressEsc)
+
+if(!evt.target.classList.contains('gallery__image')) {
+    return
+}
+const originalImg = evt.target.dataset.source;
+
+const instance = basicLightbox.create(
+    `<img src="${originalImg}" width="800" height="600">`)
+instance.show()
+
+function onPressEsc(evt){
+   if(evt.code === 'Escape'){
+    instance.close() 
+}}
+}
+
+
+
+
+
+
